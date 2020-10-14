326c8ead2b32849182e57cba4486157430af7454
Aum846 <vaghasia.aum2019@vitbhopal.ac.in>
2020-10-14 20:40:08 +0530

    views updated and file for news added

--- newsapp/news.py
+++ newsapp/news.py
@@ -1,6 +1,24 @@
+import requests
 from newsapp.models import Article, Sports, Politics, Entertainment
 
-def render_news(article):
+india_url = "https://newsapi.org/v2/everything?q=India&pageSize=100&apiKey=4f784078aec04d8d96fe1d1e1e4281bb"
+open_india_page = requests.get(india_url).json()
+article = open_india_page["articles"]
+
+sports_url = "https://newsapi.org/v2/everything?q=Sports&pageSize=100&apiKey=4f784078aec04d8d96fe1d1e1e4281bb"
+open_sports_page = requests.get(sports_url).json()
+sports_article = open_sports_page["articles"]
+
+ent_url = "https://newsapi.org/v2/everything?q=Entertainment&pageSize=100&apiKey=4f784078aec04d8d96fe1d1e1e4281bb"
+open_ent_page = requests.get(ent_url).json()
+ent_article = open_ent_page["articles"]
+
+url = "https://newsapi.org/v2/everything?q=Politics&pageSize=100&apiKey=4f784078aec04d8d96fe1d1e1e4281bb"
+news = requests.get(url).json()
+pol_article = news["articles"]
+
+
+def render_news():
     for ar in article:
         new_article = Article()
         new_article.title = ar["title"]
@@ -8,26 +26,36 @@ def render_news(article):
         new_article.img = ar["urlToImage"]
         new_article.save()
 
-def render_sports(article):
 
-    for ar in article:
+def render_sports():
+    for ar in sports_article:
         new_article = Sports()
         new_article.title = ar["title"]
         new_article.desc = ar["description"]
         new_article.img = ar["urlToImage"]
         new_article.save()
-def render_ent_news(article):
-    for ar in article:
+
+
+def render_ent_news():
+    for ar in ent_article:
         new_article = Entertainment()
         new_article.title = ar["title"]
         new_article.desc = ar["description"]
         new_article.img = ar["urlToImage"]
         new_article.save()
 
-def render_pol_news(article):
-    for ar in article:
+
+def render_pol_news():
+    for ar in pol_article:
         new_article = Politics()
         new_article.title = ar["title"]
         new_article.desc = ar["description"]
         new_article.img = ar["urlToImage"]
         new_article.save()
+
+
+if __name__ == "__main__":
+    render_news()
+    render_sports()
+    render_ent_news()
+    render_pol_news()
--- newsapp/views.py
+++ newsapp/views.py
@@ -1,7 +1,5 @@
-import requests
 from django.shortcuts import render
 from newsapp.models import Article, Sports, Entertainment, Politics
-from newsapp.news import render_news, render_pol_news, render_sports, render_ent_news
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
@@ -9,47 +7,28 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def send_politics_news(request):
-    url = "https://newsapi.org/v2/everything?q=Politics&pageSize=100&apiKey=4f784078aec04d8d96fe1d1e1e4281bb"
-    news = requests.get(url).json()
-    article = news["articles"]
-    if __name__ == "__main__":
-        render_pol_news(article)
     headline = Politics.objects.all()
     context = paginate(headline, request)
     return render(request, "newsapp/Politics.html", context)
 
+
 def send_india_news(request):
-    india_url = "https://newsapi.org/v2/everything?q=India&pageSize=100&apiKey=4f784078aec04d8d96fe1d1e1e4281bb"
-    open_india_page = requests.get(india_url).json()
-    article = open_india_page["articles"]
-    if __name__ == "__main__":
-        render_news(article)
     headline = Article.objects.all()
     context = paginate(headline, request)
-    return render(request,"newsapp/Frontpage.html", context)
+    return render(request, "newsapp/Frontpage.html", context)
 
 
 def send_sports_news(request):
-    sports_url = "https://newsapi.org/v2/everything?q=Sports&pageSize=100&apiKey=4f784078aec04d8d96fe1d1e1e4281bb"
-    open_sports_page = requests.get(sports_url).json()
-    article = open_sports_page["articles"]
-    if __name__ == "__main":
-        render_sports(article)
     headline = Sports.objects.all()
     context = paginate(headline, request)
     return render(request, "newsapp/Sports.html", context)
 
 
-
 def send_entertainment_news(request):
-    ent_url = "https://newsapi.org/v2/everything?q=Entertainment&pageSize=100&apiKey=4f784078aec04d8d96fe1d1e1e4281bb"
-    open_ent_page = requests.get(ent_url).json()
-    article = open_ent_page["articles"]
-    if __name__ == "__main__":
-        render_ent_news(article)
     headline = Entertainment.objects.all()
     context = paginate(headline, request)
     return render(request, "newsapp/Entertainment.html", context)
+
 
 def paginate(headline, request):
     paginator = Paginator(headline, 20)
@@ -64,4 +43,3 @@ def paginate(headline, request):
         'object_list': pageno,
     }
     return context
-
